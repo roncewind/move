66f8cc3f6ac3a98ad499995e5a77173aba1ec392
roncewind <dad@lynntribe.net>
2022-06-23 13:12:09 -0700

    added go routines to read/write asynchronously

--- cmd/root.go
+++ cmd/root.go
@@ -10,11 +10,14 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net"
 	// "net/http"
 	"net/url"
 	"os"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -22,6 +25,7 @@ import (
 
 var (
 	cfgFile string
+	waitGroup sync.WaitGroup
 	inputURL string
 	outputURL string
 )
@@ -43,10 +47,184 @@ to quickly create a Cobra application.`,
 		for _, key := range viper.AllKeys() {
 			fmt.Println(key, " = ", viper.Get(key))
 		}
-		read(viper.GetString("inputURL"))
-		write(viper.GetString("outputURL"))
-
+		waitGroup.Add(2)
+		valchan := make(chan string, 10)
+		go read(viper.GetString("inputURL"), valchan)
+		//writechan := make(chan string, 10)
+		go write(viper.GetString("outputURL"), valchan)
+		waitGroup.Wait()
 	},
+}
+
+// ----------------------------------------------------------------------------
+func read(urlString string, valchan chan string) {
+	fmt.Println("Enter read")
+	defer waitGroup.Done()
+	fmt.Println("Read URL:")
+	fmt.Println("URL string: ",urlString)
+	u, err := url.Parse(urlString)
+	if err != nil {
+		panic(err)
+	}
+	printURL(u)
+	if u.Scheme == "file" {
+		if strings.HasSuffix(u.Path, "jsonl") {
+			fmt.Println("Is a jsonl file.")
+			readJSONL(u.Path, valchan)
+		} else {
+			valid := validate(u.Path)
+			fmt.Println("Is valid JSON?", valid)
+			//write to channel
+		}
+	}
+}
+
+// ----------------------------------------------------------------------------
+func write(urlString string, valchan chan string) {
+	fmt.Println("Enter write")
+	defer waitGroup.Done()
+	fmt.Println("Write URL:")
+	fmt.Println("URL string: ",urlString)
+	u, err := url.Parse(urlString)
+	if err != nil {
+		panic(err)
+	}
+	printURL(u)
+
+	i := 0
+	for {
+		i++
+		// Wait for project to be assigned.
+		line, result := <- valchan
+
+		if result==false {
+			// This means the channel is empty and closed.
+			fmt.Println("valchan closed")
+			return
+		}
+
+		fmt.Println("line[", i, "]", line)
+
+		// Randomly wait to simulate work time.
+		sleep := rand.Int63n(50)
+		time.Sleep(time.Duration(sleep) * time.Millisecond)
+		// Display time to wait
+		fmt.Println("\nTime to sleep",sleep,"ms\n")
+
+	}
+}
+
+// ----------------------------------------------------------------------------
+func readJSONL(jsonFile string, valchan chan string) {
+	file, err := os.Open(jsonFile)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	i := 0
+	for scanner.Scan() {
+		i++
+		var js json.RawMessage
+		str := scanner.Text()
+		valid := json.Unmarshal([]byte(str), &js) == nil
+		if valid {
+			fmt.Println("Line", i, "is valid.")
+			valchan <- str
+		}
+	}
+	close(valchan)
+}
+
+// ----------------------------------------------------------------------------
+func validate(jsonFile string) bool {
+
+	var file *os.File = os.Stdin
+
+	if jsonFile != "" {
+		var err error
+		file, err = os.Open(jsonFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	info, err := file.Stat()
+	if err != nil {
+		panic(err)
+	}
+	if info.Size() <= 0 {
+		log.Fatal("No file found to validate.")
+	}
+	printFileInfo(info)
+
+	bytes := GetBytes(file)
+	if err := file.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	valid := json.Valid(bytes)
+	return valid
+}
+
+// ----------------------------------------------------------------------------
+func GetBytes(file *os.File) []byte {
+
+	reader := bufio.NewReader(file)
+	var output []byte
+
+	for {
+		input, err := reader.ReadByte()
+		if err != nil && err == io.EOF {
+			break
+		}
+		output = append(output, input)
+	}
+	return output
+}
+
+// ----------------------------------------------------------------------------
+func printFileInfo(info os.FileInfo) {
+	fmt.Println("name: ", info.Name())
+	fmt.Println("size: ", info.Size())
+	fmt.Println("mode: ", info.Mode())
+	fmt.Println("mod time: ", info.ModTime())
+	fmt.Println("is dir: ", info.IsDir())
+	if info.Mode() & os.ModeDevice == os.ModeDevice {
+		fmt.Println("detected device: ", os.ModeDevice)
+	}
+	if info.Mode() & os.ModeCharDevice == os.ModeCharDevice {
+		fmt.Println("detected char device: ", os.ModeCharDevice)
+	}
+	if info.Mode() & os.ModeNamedPipe == os.ModeNamedPipe {
+		fmt.Println("detected named pipe: ", os.ModeNamedPipe)
+	}
+	fmt.Println("\n")
+}
+
+// ----------------------------------------------------------------------------
+func printURL(u *url.URL) {
+
+	fmt.Println("\tScheme: ", u.Scheme)
+	fmt.Println("\tUser full: ", u.User)
+	fmt.Println("\tUser name: ", u.User.Username())
+	p, _ := u.User.Password()
+	fmt.Println("\tPassword: ", p)
+
+	fmt.Println("\tHost full: ", u.Host)
+	host, port, _ := net.SplitHostPort(u.Host)
+	fmt.Println("\tHost: ", host)
+	fmt.Println("\tPort: ", port)
+
+	fmt.Println("\tPath: ", u.Path)
+	fmt.Println("\tFragment: ", u.Fragment)
+
+	fmt.Println("\tQuery string: ", u.RawQuery)
+	m, _ := url.ParseQuery(u.RawQuery)
+	fmt.Println("\tParsed query string: ", m)
+	// fmt.Println(m["k"][0])
 }
 
 // ----------------------------------------------------------------------------
@@ -107,151 +285,4 @@ func initConfig() {
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	}
-}
-
-// ----------------------------------------------------------------------------
-func printURL(u *url.URL) {
-
-	fmt.Println("\tScheme: ", u.Scheme)
-	fmt.Println("\tUser full: ", u.User)
-	fmt.Println("\tUser name: ", u.User.Username())
-	p, _ := u.User.Password()
-	fmt.Println("\tPassword: ", p)
-
-	fmt.Println("\tHost full: ", u.Host)
-	host, port, _ := net.SplitHostPort(u.Host)
-	fmt.Println("\tHost: ", host)
-	fmt.Println("\tPort: ", port)
-
-	fmt.Println("\tPath: ", u.Path)
-	fmt.Println("\tFragment: ", u.Fragment)
-
-	fmt.Println("\tQuery string: ", u.RawQuery)
-	m, _ := url.ParseQuery(u.RawQuery)
-	fmt.Println("\tParsed query string: ", m)
-	// fmt.Println(m["k"][0])
-}
-
-// ----------------------------------------------------------------------------
-func read(urlString string) {
-	fmt.Println("Enter read")
-	fmt.Println("Read URL:")
-	fmt.Println("URL string: ",urlString)
-	u, err := url.Parse(urlString)
-	if err != nil {
-		panic(err)
-	}
-	printURL(u)
-	if u.Scheme == "file" {
-		if strings.HasSuffix(u.Path, "jsonl") {
-			fmt.Println("Is a jsonl file.")
-			readJSONL(u.Path)
-		} else {
-			valid := validate(u.Path)
-			fmt.Println("Is valid JSON?", valid)
-			//write to channel
-		}
-	}
-}
-
-// ----------------------------------------------------------------------------
-func write(urlString string) {
-	fmt.Println("Enter write")
-	fmt.Println("Write URL:")
-	fmt.Println("URL string: ",urlString)
-	u, err := url.Parse(urlString)
-	if err != nil {
-		panic(err)
-	}
-	printURL(u)
-}
-
-// ----------------------------------------------------------------------------
-func readJSONL(jsonFile string) {
-	file, err := os.Open(jsonFile)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanLines)
-
-	i := 0
-	for scanner.Scan() {
-		i++
-		var js json.RawMessage
-		str := scanner.Text()
-		valid := json.Unmarshal([]byte(str), &js) == nil
-		if valid {
-
-		}
-		fmt.Println(i, "Is valid?", valid)
-	}
-
-}
-
-// ----------------------------------------------------------------------------
-func validate(jsonFile string) bool {
-
-	var file *os.File = os.Stdin
-
-	if jsonFile != "" {
-		var err error
-		file, err = os.Open(jsonFile)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	info, err := file.Stat()
-	if err != nil {
-		panic(err)
-	}
-	if info.Size() <= 0 {
-		log.Fatal("No file found to validate.")
-	}
-	PrintFileInfo(info)
-
-	bytes := GetBytes(file)
-	if err := file.Close(); err != nil {
-		log.Fatal(err)
-	}
-
-	valid := json.Valid(bytes)
-	return valid
-}
-
-// ----------------------------------------------------------------------------
-func GetBytes(file *os.File) []byte {
-
-	reader := bufio.NewReader(file)
-	var output []byte
-
-	for {
-		input, err := reader.ReadByte()
-		if err != nil && err == io.EOF {
-			break
-		}
-		output = append(output, input)
-	}
-	return output
-}
-
-// ----------------------------------------------------------------------------
-func PrintFileInfo(info os.FileInfo) {
-	fmt.Println("name: ", info.Name())
-	fmt.Println("size: ", info.Size())
-	fmt.Println("mode: ", info.Mode())
-	fmt.Println("mod time: ", info.ModTime())
-	fmt.Println("is dir: ", info.IsDir())
-	if info.Mode() & os.ModeDevice == os.ModeDevice {
-		fmt.Println("detected device: ", os.ModeDevice)
-	}
-	if info.Mode() & os.ModeCharDevice == os.ModeCharDevice {
-		fmt.Println("detected char device: ", os.ModeCharDevice)
-	}
-	if info.Mode() & os.ModeNamedPipe == os.ModeNamedPipe {
-		fmt.Println("detected named pipe: ", os.ModeNamedPipe)
-	}
-	fmt.Println("\n")
 }
