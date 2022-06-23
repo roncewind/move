5c0076f3a17e612ac46796e17a56551ccde7fba9
roncewind <dad@lynntribe.net>
2022-06-23 12:29:37 -0700

    added file reading

--- cmd/root.go
+++ cmd/root.go
@@ -1,21 +1,33 @@
 /*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+Copyright © 2022 Ron Lynn <dad@lynntribe.net>
 
 */
 package cmd
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net"
+	// "net/http"
+	"net/url"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
-var cfgFile string
+var (
+	cfgFile string
+	inputURL string
+	outputURL string
+)
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
+// RootCmd represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
 	Use:   "move",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
@@ -26,18 +38,28 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("start Run")
+		for _, key := range viper.AllKeys() {
+			fmt.Println(key, " = ", viper.Get(key))
+		}
+		read(viper.GetString("inputURL"))
+		write(viper.GetString("outputURL"))
+
+	},
 }
 
+// ----------------------------------------------------------------------------
 // Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
+// This is called by main.main(). It only needs to happen once to the RootCmd.
 func Execute() {
-	err := rootCmd.Execute()
+	err := RootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
 }
 
+// ----------------------------------------------------------------------------
 func init() {
 	cobra.OnInitialize(initConfig)
 
@@ -45,13 +67,18 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.move.yaml)")
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.senzing/config.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	RootCmd.Flags().StringVarP(&inputURL, "inputURL", "i", "", "input location")
+	viper.BindPFlag("inputURL", RootCmd.Flags().Lookup("inputURL"))
+	RootCmd.Flags().StringVarP(&outputURL, "outputURL", "o", "", "ouput location")
+	viper.BindPFlag("outputURL", RootCmd.Flags().Lookup("outputURL"))
 }
 
+// ----------------------------------------------------------------------------
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	if cfgFile != "" {
@@ -62,16 +89,169 @@ func initConfig() {
 		home, err := os.UserHomeDir()
 		cobra.CheckErr(err)
 
-		// Search config in home directory with name ".move" (without extension).
+		// Search config in <home directory>/.senzing with name "config" (without extension).
+		viper.AddConfigPath(home+"/.senzing-tools")
 		viper.AddConfigPath(home)
+		viper.AddConfigPath("/etc/senzing-tools")
 		viper.SetConfigType("yaml")
-		viper.SetConfigName(".move")
+		viper.SetConfigName("config")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
+	// all env vars should be prefixed with "SENZING_TOOLS_"
+	viper.SetEnvPrefix("senzing_tools")
+	viper.BindEnv("inputURL")
+	viper.BindEnv("outputURL")
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	}
+}
+
+// ----------------------------------------------------------------------------
+func printURL(u *url.URL) {
+
+	fmt.Println("\tScheme: ", u.Scheme)
+	fmt.Println("\tUser full: ", u.User)
+	fmt.Println("\tUser name: ", u.User.Username())
+	p, _ := u.User.Password()
+	fmt.Println("\tPassword: ", p)
+
+	fmt.Println("\tHost full: ", u.Host)
+	host, port, _ := net.SplitHostPort(u.Host)
+	fmt.Println("\tHost: ", host)
+	fmt.Println("\tPort: ", port)
+
+	fmt.Println("\tPath: ", u.Path)
+	fmt.Println("\tFragment: ", u.Fragment)
+
+	fmt.Println("\tQuery string: ", u.RawQuery)
+	m, _ := url.ParseQuery(u.RawQuery)
+	fmt.Println("\tParsed query string: ", m)
+	// fmt.Println(m["k"][0])
+}
+
+// ----------------------------------------------------------------------------
+func read(urlString string) {
+	fmt.Println("Enter read")
+	fmt.Println("Read URL:")
+	fmt.Println("URL string: ",urlString)
+	u, err := url.Parse(urlString)
+	if err != nil {
+		panic(err)
+	}
+	printURL(u)
+	if u.Scheme == "file" {
+		if strings.HasSuffix(u.Path, "jsonl") {
+			fmt.Println("Is a jsonl file.")
+			readJSONL(u.Path)
+		} else {
+			valid := validate(u.Path)
+			fmt.Println("Is valid JSON?", valid)
+			//write to channel
+		}
+	}
+}
+
+// ----------------------------------------------------------------------------
+func write(urlString string) {
+	fmt.Println("Enter write")
+	fmt.Println("Write URL:")
+	fmt.Println("URL string: ",urlString)
+	u, err := url.Parse(urlString)
+	if err != nil {
+		panic(err)
+	}
+	printURL(u)
+}
+
+// ----------------------------------------------------------------------------
+func readJSONL(jsonFile string) {
+	file, err := os.Open(jsonFile)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	i := 0
+	for scanner.Scan() {
+		i++
+		var js json.RawMessage
+		str := scanner.Text()
+		valid := json.Unmarshal([]byte(str), &js) == nil
+		if valid {
+
+		}
+		fmt.Println(i, "Is valid?", valid)
+	}
+
+}
+
+// ----------------------------------------------------------------------------
+func validate(jsonFile string) bool {
+
+	var file *os.File = os.Stdin
+
+	if jsonFile != "" {
+		var err error
+		file, err = os.Open(jsonFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	info, err := file.Stat()
+	if err != nil {
+		panic(err)
+	}
+	if info.Size() <= 0 {
+		log.Fatal("No file found to validate.")
+	}
+	PrintFileInfo(info)
+
+	bytes := GetBytes(file)
+	if err := file.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	valid := json.Valid(bytes)
+	return valid
+}
+
+// ----------------------------------------------------------------------------
+func GetBytes(file *os.File) []byte {
+
+	reader := bufio.NewReader(file)
+	var output []byte
+
+	for {
+		input, err := reader.ReadByte()
+		if err != nil && err == io.EOF {
+			break
+		}
+		output = append(output, input)
+	}
+	return output
+}
+
+// ----------------------------------------------------------------------------
+func PrintFileInfo(info os.FileInfo) {
+	fmt.Println("name: ", info.Name())
+	fmt.Println("size: ", info.Size())
+	fmt.Println("mode: ", info.Mode())
+	fmt.Println("mod time: ", info.ModTime())
+	fmt.Println("is dir: ", info.IsDir())
+	if info.Mode() & os.ModeDevice == os.ModeDevice {
+		fmt.Println("detected device: ", os.ModeDevice)
+	}
+	if info.Mode() & os.ModeCharDevice == os.ModeCharDevice {
+		fmt.Println("detected char device: ", os.ModeCharDevice)
+	}
+	if info.Mode() & os.ModeNamedPipe == os.ModeNamedPipe {
+		fmt.Println("detected named pipe: ", os.ModeNamedPipe)
+	}
+	fmt.Println("\n")
 }
