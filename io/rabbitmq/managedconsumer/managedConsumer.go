f0924bfb9e688923b33186459ecfb18fb16f8956
roncewind <dad@lynntribe.net>
2023-03-08 13:00:39 -0800

    save point

--- io/rabbitmq/managedconsumer/managedConsumer.go
+++ io/rabbitmq/managedconsumer/managedConsumer.go
@@ -101,7 +101,7 @@ func (j *RabbitJob) OnError(err error) {
 // the given queue.
 // - Workers restart when they are killed or die.
 // - respond to standard system signals.
-func StartManagedConsumer(ctx context.Context, urlString string, numberOfWorkers int, createG2Engine func(context.Context) *g2api.G2engine, withInfo bool) chan struct{} {
+func StartManagedConsumer(ctx context.Context, urlString string, numberOfWorkers int, g2engine *g2api.G2engine, withInfo bool) chan struct{} {
 
 	//default to the max number of OS threads
 	if numberOfWorkers <= 0 {
@@ -116,7 +116,7 @@ func StartManagedConsumer(ctx context.Context, urlString string, numberOfWorkers
 	jobPool = make(chan *RabbitJob, numberOfWorkers)
 	for i := 0; i < numberOfWorkers; i++ {
 		jobPool <- &RabbitJob{
-			engine:    createG2Engine(ctx),
+			engine:    g2engine,
 			id:        i,
 			usedCount: 0,
 			withInfo:  withInfo,
@@ -142,7 +142,6 @@ func StartManagedConsumer(ctx context.Context, urlString string, numberOfWorkers
 		ok := true
 		for ok {
 			job, ok = <-jobPool
-			(*job.engine).Destroy(ctx)
 			fmt.Println("Job:", job.id, "used:", job.usedCount)
 		}
 	}
