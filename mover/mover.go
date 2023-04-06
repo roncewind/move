3f922eda6dfd84691cbf995f2c8640e42913bcda
roncewind <dad@lynntribe.net>
2023-04-06 08:10:25 -0700

    refactored and commented move code

--- mover/mover.go
+++ mover/mover.go
@@ -61,6 +61,39 @@ func (m *MoverImpl) Move(ctx context.Context) {
 }
 
 // ----------------------------------------------------------------------------
+// -- Write implementation: writes records in the record channel to the output
+// ----------------------------------------------------------------------------
+
+// this function implements writing to RabbitMQ
+func (m *MoverImpl) write(ctx context.Context, recordchan chan queues.Record) {
+	fmt.Println("Enter write")
+	defer waitGroup.Done()
+	fmt.Println("Write URL string: ", m.OutputURL)
+	u, err := url.Parse(m.OutputURL)
+	if err != nil {
+		panic(err)
+	}
+	printURL(u)
+
+	// set number of workers to runtime.GOMAXPROCS(0)
+	productionChan, startErr := managedproducer.StartManagedProducer(ctx, m.OutputURL, runtime.GOMAXPROCS(0), recordchan)
+	if startErr != nil {
+		msg := "there was an unexpected issue; please report this as a bug."
+		if _, ok := startErr.(managedproducer.ManagedProducerError); ok {
+			msg = "unable to start the managed producer"
+		}
+		panic(MoverError{util.WrapError(startErr, msg)})
+	}
+	<-util.OrDone(ctx, productionChan)
+	fmt.Println("So long and thanks for all the fish.")
+}
+
+// ----------------------------------------------------------------------------
+// -- Read implementation: reads records from the input to the record channel
+// ----------------------------------------------------------------------------
+
+// this function attempts to determine the source of records.
+// it then parses the source and puts the records into the record channel.
 func (m *MoverImpl) read(ctx context.Context, recordchan chan queues.Record) {
 
 	defer waitGroup.Done()
@@ -105,30 +138,41 @@ func (m *MoverImpl) read(ctx context.Context, recordchan chan queues.Record) {
 }
 
 // ----------------------------------------------------------------------------
-func (m *MoverImpl) write(ctx context.Context, recordchan chan queues.Record) {
-	fmt.Println("Enter write")
-	defer waitGroup.Done()
-	fmt.Println("Write URL string: ", m.OutputURL)
-	u, err := url.Parse(m.OutputURL)
-	if err != nil {
-		panic(err)
-	}
-	printURL(u)
 
-	// set number of workers to runtime.GOMAXPROCS(0)
-	productionChan, startErr := managedproducer.StartManagedProducer(ctx, m.OutputURL, runtime.GOMAXPROCS(0), recordchan)
-	if startErr != nil {
-		msg := "there was an unexpected issue; please report this as a bug."
-		if _, ok := startErr.(managedproducer.ManagedProducerError); ok {
-			msg = "unable to start the managed producer"
+// process records in the JSONL format; reading one record per line from
+// the given reader and placing the records into the record channel
+func processJSONL(fileName string, reader io.Reader, recordchan chan queues.Record) {
+
+	fmt.Println(time.Now(), "Start file read", fileName)
+
+	scanner := bufio.NewScanner(reader)
+	scanner.Split(bufio.ScanLines)
+
+	i := 0
+	for scanner.Scan() {
+		i++
+		str := strings.TrimSpace(scanner.Text())
+		// ignore blank lines
+		if len(str) > 0 {
+			valid, err := record.Validate(str)
+			if valid {
+				recordchan <- &szRecord{str, fileName, i}
+			} else {
+				fmt.Println("Line", i, err)
+			}
 		}
-		panic(MoverError{util.WrapError(startErr, msg)})
+		if i%10000 == 0 {
+			fmt.Println(time.Now(), "Records sent to queue:", i)
+		}
 	}
-	<-util.OrDone(ctx, productionChan)
-	fmt.Println("So long and thanks for all the fish.")
+	close(recordchan)
+
+	fmt.Println(time.Now(), "Record channel close for file", fileName)
 }
 
 // ----------------------------------------------------------------------------
+
+// opens and reads a JSONL http resource
 func readJSONLResource(jsonURL string, recordchan chan queues.Record) {
 	response, err := http.Get(jsonURL)
 	if err != nil {
@@ -136,32 +180,12 @@ func readJSONLResource(jsonURL string, recordchan chan queues.Record) {
 	}
 	defer response.Body.Close()
 
-	scanner := bufio.NewScanner(response.Body)
-	scanner.Split(bufio.ScanLines)
-
-	fmt.Println(time.Now(), "Start resource read", jsonURL)
-	i := 0
-	for scanner.Scan() {
-		i++
-		str := strings.TrimSpace(scanner.Text())
-		// ignore blank lines
-		if len(str) > 0 {
-			valid, err := record.Validate(str)
-			if valid {
-				recordchan <- &szRecord{str, jsonURL, i}
-			} else {
-				fmt.Println("Line", i, err)
-			}
-		}
-		if i%10000 == 0 {
-			fmt.Println(time.Now(), "Records sent to queue:", i)
-		}
-	}
-	close(recordchan)
-	fmt.Println(time.Now(), "Record channel close for resource", jsonURL)
+	processJSONL(jsonURL, response.Body, recordchan)
 }
 
 // ----------------------------------------------------------------------------
+
+// opens and reads a JSONL file
 func readJSONLFile(jsonFile string, recordchan chan queues.Record) {
 	file, err := os.Open(jsonFile)
 	if err != nil {
@@ -169,32 +193,12 @@ func readJSONLFile(jsonFile string, recordchan chan queues.Record) {
 	}
 	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanLines)
-
-	fmt.Println(time.Now(), "Start file read", jsonFile)
-	i := 0
-	for scanner.Scan() {
-		i++
-		str := strings.TrimSpace(scanner.Text())
-		// ignore blank lines
-		if len(str) > 0 {
-			valid, err := record.Validate(str)
-			if valid {
-				recordchan <- &szRecord{str, jsonFile, i}
-			} else {
-				fmt.Println("Line", i, err)
-			}
-		}
-		if i%10000 == 0 {
-			fmt.Println(time.Now(), "Records sent to queue:", i)
-		}
-	}
-	close(recordchan)
-	fmt.Println(time.Now(), "Record channel close for file", jsonFile)
+	processJSONL(jsonFile, file, recordchan)
 }
 
 // ----------------------------------------------------------------------------
+
+// opens and reads a JSONL file that has been Gzipped
 func readGZFile(gzFile string, recordchan chan queues.Record) {
 	gzipfile, err := os.Open(gzFile)
 	if err != nil {
@@ -208,32 +212,13 @@ func readGZFile(gzFile string, recordchan chan queues.Record) {
 	}
 	defer reader.Close()
 
-	scanner := bufio.NewScanner(reader)
-	scanner.Split(bufio.ScanLines)
-
-	fmt.Println(time.Now(), "Start file read", gzFile)
-	i := 0
-	for scanner.Scan() {
-		i++
-		str := strings.TrimSpace(scanner.Text())
-		// ignore blank lines
-		if len(str) > 0 {
-			valid, err := record.Validate(str)
-			if valid {
-				recordchan <- &szRecord{str, gzFile, i}
-			} else {
-				fmt.Println("Line", i, err)
-			}
-		}
-		if i%10000 == 0 {
-			fmt.Println(time.Now(), "Records sent to queue:", i)
-		}
-	}
-	close(recordchan)
-	fmt.Println(time.Now(), "Record channel close for file", gzFile)
+	processJSONL(gzFile, reader, recordchan)
 }
 
 // ----------------------------------------------------------------------------
+
+// validates that a file is valid JSON
+// TODO:  implement loading of a JSON file.  what is the actual JSON format?
 func validate(jsonFile string) bool {
 
 	var file *os.File = os.Stdin
@@ -254,7 +239,7 @@ func validate(jsonFile string) bool {
 	}
 	printFileInfo(info)
 
-	bytes := GetBytes(file)
+	bytes := getBytes(file)
 	if err := file.Close(); err != nil {
 		log.Fatal(err)
 	}
@@ -264,7 +249,10 @@ func validate(jsonFile string) bool {
 }
 
 // ----------------------------------------------------------------------------
-func GetBytes(file *os.File) []byte {
+
+// used for validating a JSON file
+// TODO:  this seems like a naive implementation.  What if the file is very large?
+func getBytes(file *os.File) []byte {
 
 	reader := bufio.NewReader(file)
 	var output []byte
@@ -280,6 +268,9 @@ func GetBytes(file *os.File) []byte {
 }
 
 // ----------------------------------------------------------------------------
+
+// print basic file information.
+// TODO:  should this info be logged?  DELETE ME?
 func printFileInfo(info os.FileInfo) {
 	fmt.Println("name: ", info.Name())
 	fmt.Println("size: ", info.Size())
@@ -298,6 +289,9 @@ func printFileInfo(info os.FileInfo) {
 }
 
 // ----------------------------------------------------------------------------
+
+// print out basic URL information.
+// TODO:  should this info be logged?  DELETE ME?
 func printURL(u *url.URL) {
 
 	fmt.Println("\tScheme: ", u.Scheme)
@@ -324,7 +318,7 @@ func printURL(u *url.URL) {
 }
 
 // ----------------------------------------------------------------------------
-// record implementation
+// record implementation: provides a raw data record implementation
 // ----------------------------------------------------------------------------
 
 type szRecord struct {
