ee153c28a0951a418ebc5a3a3bb38c1a19b5cf5a
roncewind <dad@lynntribe.net>
2023-05-19 11:07:23 -0700

    save point

--- io/sqs/sqs.go
+++ io/sqs/sqs.go
@@ -2,11 +2,9 @@ package sqs
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/roncewind/go-util/queues"
-	"github.com/roncewind/go-util/queues/sqs"
-	"github.com/roncewind/go-util/util"
+	"github.com/roncewind/move/io/sqs/managedconsumer"
 	"github.com/roncewind/move/io/sqs/managedproducer"
 	"github.com/senzing/g2-sdk-go/g2api"
 )
@@ -24,39 +22,39 @@ func StartProducer(ctx context.Context, urlString string, numberOfWorkers int, r
 
 // Start processing records in SQS
 func StartConsumer(ctx context.Context, urlString string, numberOfWorkers int, g2engine *g2api.G2engine, withInfo bool) (err error) {
-	// return managedconsumer.StartManagedConsumer(ctx, urlString, numberOfWorkers, g2engine, withInfo)
+	return managedconsumer.StartManagedConsumer(ctx, urlString, numberOfWorkers, g2engine, withInfo)
 
-	fmt.Println("Get new sqs client")
-	client, err := sqs.NewClient(ctx, urlString)
-	if err != nil {
-		fmt.Println("SQS new client error:", err)
-		return
-	}
-	fmt.Println("SQS client:", client)
-	msgChan, err := client.Consume(ctx)
-	if err != nil {
-		fmt.Println("SQS consume error:", err)
-		return
-	}
-	for record := range util.OrDone(ctx, msgChan) {
+	// fmt.Println("Get new sqs client")
+	// client, err := sqs.NewClient(ctx, urlString)
+	// if err != nil {
+	// 	fmt.Println("SQS new client error:", err)
+	// 	return
+	// }
+	// fmt.Println("SQS client:", client)
+	// msgChan, err := client.Consume(ctx)
+	// if err != nil {
+	// 	fmt.Println("SQS consume error:", err)
+	// 	return
+	// }
+	// for record := range util.OrDone(ctx, msgChan) {
 
-		// fmt.Println("Record address", record)
-		fmt.Println("Record MessageBody:", *record.Body)
-		//TODO: added Senzing here
-		//TODO: watch how long processing is taking and update the visibility timeout
+	// 	// fmt.Println("Record address", record)
+	// 	fmt.Println("Record MessageBody:", *record.Body)
+	// 	//TODO: added Senzing here
+	// 	//TODO: watch how long processing is taking and update the visibility timeout
 
-		//TODO: on error add message to Dead Letter Queue
-		// err := client.PushDeadRecord(ctx, record)
-		// if err != nil {
-		// 	fmt.Println("Error pushing message to the dead letter queue", record)
-		// }
-		//TODO:  ignore error for dead records?
+	// 	//TODO: on error add message to Dead Letter Queue
+	// 	// err := client.PushDeadRecord(ctx, record)
+	// 	// if err != nil {
+	// 	// 	fmt.Println("Error pushing message to the dead letter queue", record)
+	// 	// }
+	// 	//TODO:  ignore error for dead records?
 
-		//as long as there was no error delete the message from the queue
-		err = client.RemoveMessage(ctx, &record)
-		if err != nil {
-			fmt.Println("Error removing message", record)
-		}
-	}
-	return nil
+	// 	//as long as there was no error delete the message from the queue
+	// 	err = client.RemoveMessage(ctx, &record)
+	// 	if err != nil {
+	// 		fmt.Println("Error removing message", record)
+	// 	}
+	// }
+	// return nil
 }
